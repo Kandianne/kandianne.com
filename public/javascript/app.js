63f8898db98c087ad93a60ac7b6a84d55ab067b8
kandianne <kandipierre@gmail.com>
2015-10-23 18:44:29 -0700

    redesign

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -4,7 +4,7 @@
 	.config(Config)
 	.config(function($mdThemingProvider) {
 		$mdThemingProvider.theme('default')
-		.primaryPalette('blue-grey',{
+		.primaryPalette('grey',{
 			'default': '100', // by default use shade 400 from the pink palette for primary intentions
       'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
       'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
@@ -27,8 +27,6 @@
 			templateUrl: 'templates/contact.html',
 		}).state('Portfolio',{
 			templateUrl: 'templates/portfolio.html',
-		}).state('Responsive',{
-			templateUrl: 'templates/experience2.html',
 		});
 		$urlRouterProvider.otherwise('/');
 	}
